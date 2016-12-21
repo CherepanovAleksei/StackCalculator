3895fd7b5874f2e0601906dbb6e189ff8106357d
mrneumann <mrneumann@localhost.localdomain>
2016-12-21 11:10:31 +0300

    fix input

--- main.c
+++ main.c
@@ -18,19 +18,47 @@ int main()
         switch (c)
         {
         case '+':
+            if(dbll->head && dbll->head->first->next)
+            {
             if_sum(dbll);
+            }
+            else
+            {
+              printf("Fewarguments, empty stack!\n");
+            }
             break;
         case '-':
             minus++;
             break;
         case '*':
-            if_multiplication(dbll);
+            if(dbll->head && dbll->head->first->next)
+            {
+                if_multiplication(dbll);
+            }
+            else
+            {
+              printf("Fewarguments, empty stack!\n");
+            }
             break;
         case '/':
-            if_division(dbll);
+            if(dbll->head && dbll->head->first->next)
+            {
+                if_division(dbll);
+            }
+            else
+            {
+              printf("Few arguments, empty stack!\n");
+            }
             break;
         case 'd':
-            number_delete(dbll);
+            if(dbll->head)
+            {
+                number_delete(dbll);
+            }
+            else
+            {
+                printf("Nothing to output!!!\n");
+            }
             break;
         case '=':
             if(dbll->head)
@@ -53,11 +81,17 @@ int main()
             {
                 if(minus)
                 {
-                    if_subtraction(dbll);
+                    if(dbll->head && dbll->head->first->next)
+                    {
+                        if_subtraction(dbll);
+                    }
+                    else
+                    {
+                        printf("Few arguments, empty stack!\n");
+                    }
                 }
                 flag=0;
                 minus=0;
-                printf("принято\n");
             }
             break;
         }
